c4e4581e724df84d32cefa19793de22cb7560c75
Trey Aughenbaugh <treyaughenbaugh@gmail.com>
2023-09-02 02:42:32 -0400

    update test

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 /*
 https://github.com/websockets/ws/tree/master
 */
-const version = '1.0';
+const version = '1.1';
 const http = require('http');
 const express = require('express');
 const handlebars = require('express-handlebars');
